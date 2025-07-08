a68b0193217f139881522f38ac056deda1390e0b
Rony <ronyjavierramirezalvarado@gmail.com>
2025-07-08 09:49:25 -0600

    v3

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ import cookieParser from 'cookie-parser'
 
 //Rutas de uso
 import clientsRoute from './src/routes/clients.js'
+import bookingRoute from './src/routes/booking.js'
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/clients', clientsRoute)
+app.use('/api/booking', bookingRoute)
 
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Endpoint not found' })
---
+++ backend/src/routes/booking.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bookingControl from '../controller/bookingControl.js';
+
+const router = express.Router()
+
+router.route('/').get(bookingControl.get)
+router.route('/').post(bookingControl.post)
+
+router.route('/:id').get(bookingControl.get)
+router.route('/:id').put(bookingControl.put)
+router.route('/:id').delete(bookingControl.delete)
+
+export default router;
