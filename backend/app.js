1cdbe5a6b5430b74057da87412a97be836c365e9
Rony <ronyjavierramirezalvarado@gmail.com>
2025-07-08 11:21:24 -0600

    v5

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,30 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import cookieParser from "cookie-parser";
 
 //Rutas de uso
-import clientsRoute from './src/routes/clients.js'
-import bookingRoute from './src/routes/booking.js'
+import clientsRoute from "./src/routes/clients.js";
+import bookingRoute from "./src/routes/booking.js";
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use('/api/clients', clientsRoute)
-app.use('/api/booking', bookingRoute)
+//Importaciones para el uso de swaggerUi
+
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve("./documentacion.json"), "utf-8")
+);
+
+app.use("/api/clients", clientsRoute);
+app.use("/api/booking", bookingRoute);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res, next) => {
-    res.status(404).json({ message: 'Endpoint not found' })
-})
+  res.status(404).json({ message: "Endpoint not found" });
+});
 
 export default app;
